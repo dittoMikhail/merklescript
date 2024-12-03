85251aa47613c1da8d7d5c2ace7009ca5691ed93
davydovMikhail <davydov27977@gmail.com>
2024-12-03 11:06:04 +0300

    script

--- dist/index.js
+++ dist/index.js
@@ -9,29 +9,44 @@ const units_1 = require("@ethersproject/units");
 const ethers_1 = require("ethers");
 let Tree;
 let RootTree;
+const addresses = [
+    '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+    '0x90F79bf6EB2c4f870365E785982E1f101E93b906',
+    '0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65'
+];
+const amounts = [
+    (0, units_1.parseEther)('10000'),
+    (0, units_1.parseEther)('20000'),
+    (0, units_1.parseEther)('40000'),
+];
 function getLeafs(addresses, amounts) {
     let abiCoder = new ethers_1.utils.AbiCoder();
     const leafs = addresses.map((address, i) => (0, keccak256_1.default)(abiCoder.encode(["address", "uint"], [address, amounts[i]._hex])));
     return leafs;
+}
+function getLeaf(address, amount) {
+    let abiCoder = new ethers_1.utils.AbiCoder();
+    const leaf = (0, keccak256_1.default)(abiCoder.encode(["address", "uint"], [address, amount._hex]));
+    return leaf;
 }
 function createMerkleTree(_leafs) {
     Tree = new merkletreejs_1.MerkleTree(_leafs, keccak256_1.default, { sortPairs: true });
     RootTree = Tree.getHexRoot();
 }
 function writeRootTree() {
-    const addresses = [
-        "0xB4f54ecfa062dF6216EBb2D6C76e852Fc46Edf99",
-        "0xf60A421B87d07Bc8dFCB4603169797668227f0dB",
-        "0x9b83f193B781cec168f6B1540C6A488dA2Bfb364"
-    ];
-    const amounts = [
-        (0, units_1.parseEther)('10000'),
-        (0, units_1.parseEther)('20000'),
-        (0, units_1.parseEther)('40000'),
-    ];
     let leafs = getLeafs(addresses, amounts);
+    // console.log(leafs);
     createMerkleTree(leafs);
     RootTree = Tree.getHexRoot();
     console.log(RootTree);
 }
+function showProofArray() {
+    const proofsOne = Tree.getHexProof(getLeaf(addresses[0], amounts[0]));
+    console.log("Proofs 1", proofsOne);
+    const proofsTwo = Tree.getHexProof(getLeaf(addresses[1], amounts[1]));
+    console.log("Proofs 2", proofsTwo);
+    const proofsThree = Tree.getHexProof(getLeaf(addresses[2], amounts[2]));
+    console.log("Proofs 3", proofsThree);
+}
 writeRootTree();
+showProofArray();
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,27 @@ import { BigNumber, utils } from "ethers";
 let Tree: MerkleTree;
 let RootTree: string;
 
+const addresses = [
+    '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+    '0x90F79bf6EB2c4f870365E785982E1f101E93b906',
+    '0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65'
+]
+const amounts = [
+    parseEther('10000'),
+    parseEther('20000'),
+    parseEther('40000'),
+]
+
 function getLeafs(addresses: string[], amounts: BigNumber[]) {
     let abiCoder = new utils.AbiCoder();
     const leafs = addresses.map((address, i) => keccak256( abiCoder.encode([ "address", "uint" ], [address, amounts[i]._hex]))); 
     return leafs;
+}
+
+function getLeaf(address: string, amount: BigNumber) {
+    let abiCoder = new utils.AbiCoder();
+    const leaf = keccak256(abiCoder.encode([ "address", "uint" ], [address, amount._hex])); 
+    return leaf;
 }
 
 function createMerkleTree(_leafs: Buffer[]) {
@@ -18,21 +35,20 @@ function createMerkleTree(_leafs: Buffer[]) {
 }
 
 function writeRootTree() {
-    const addresses = [
-        "0xB4f54ecfa062dF6216EBb2D6C76e852Fc46Edf99",
-        "0xf60A421B87d07Bc8dFCB4603169797668227f0dB",
-        "0x9b83f193B781cec168f6B1540C6A488dA2Bfb364"
-    ];
-    const amounts = [
-        parseEther('10000'),
-        parseEther('20000'),
-        parseEther('40000'),
-    ]
-
     let leafs = getLeafs(addresses, amounts);
     createMerkleTree(leafs);
     RootTree = Tree.getHexRoot();
     console.log(RootTree);
 }
 
-writeRootTree()
+function showProofArray() {
+    const proofsOne = Tree.getHexProof(getLeaf(addresses[0], amounts[0]));
+    console.log("Proofs 1", proofsOne);
+    const proofsTwo = Tree.getHexProof(getLeaf(addresses[1], amounts[1]));
+    console.log("Proofs 2", proofsTwo);
+    const proofsThree = Tree.getHexProof(getLeaf(addresses[2], amounts[2]));
+    console.log("Proofs 3", proofsThree);
+}
+
+writeRootTree();
+showProofArray();
